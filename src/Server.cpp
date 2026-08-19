e7be0e7dc6b17845f401c850a4731264a80b00e5
Nathan Baines <nathan.baines@nathcat.net>
2026-08-19 15:49:03 +0100

    Removed form data from logging

--- src/Server.cpp
+++ src/Server.cpp
@@ -2,17 +2,11 @@
 using namespace nathcat::api;
 
 nathcat::api::Server::Server() {
-  http.set_logger([](const httplib::Request &req,
-                     const httplib::Response &res) {
-    std::cout << req.method << " " << req.path << " -> " << res.status
-              << std::endl;
-
-    std::cout << "FORM DATA -----" << std::endl;
-    for (auto i = req.form.fields.begin(); i != req.form.fields.end(); i++) {
-      std::cout << i->first << " -> " << i->second.content << std::endl;
-    }
-    std::cout << "---------------" << std::endl;
-  });
+  http.set_logger(
+      [](const httplib::Request &req, const httplib::Response &res) {
+        std::cout << req.method << " " << req.path << " -> " << res.status
+                  << std::endl;
+      });
 }
 
 void nathcat::api::Server::registerEndpoint(struct endpoint e) {
